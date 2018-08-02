0f50b175dce0f44930118f85bef25e119030a79a
leviboenish <leviboenish@gmail.com>
2018-08-02 11:07:08 -0600

    port fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 7000;
+const port = process.env.PORT || 7000;
 const queries = require('./queries')
 
 app.get('/', (req,res,next) =>{
